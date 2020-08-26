73543e57288aee1dfa9d24cce4f84e3bc82f8355
Cherryello <felicedarioceriello@gmail.com>
2020-08-26 15:48:32 +0200

    Aggiustata animazione di quanto atterri sull'erba dopo il surf + messa una funzione tolta in FRLG e non rimessa su Smeraldo

--- src/main.c
+++ src/main.c
@@ -75,7 +75,7 @@ static EWRAM_DATA u16 gTrainerId = 0;
 static void UpdateLinkAndCallCallbacks(void);
 static void InitMainCallbacks(void);
 static void CallCallbacks(void);
-//static void SeedRngWithRtc(void);
+static void SeedRngWithRtc(void);
 static void ReadKeys(void);
 void InitIntrHandlers(void);
 static void WaitForVBlank(void);
@@ -119,7 +119,7 @@ void AgbMain()
     InitMainCallbacks();
     InitMapMusic();
 	SeedRngAndSetTrainerId();
-    //SeedRngWithRtc(); see comment at SeedRngWithRtc declaration below
+    SeedRngWithRtc();
     ClearDma3Requests();
     ResetBgs();
     SetDefaultFontsPointer();
@@ -232,13 +232,13 @@ void EnableVCountIntrAtLine150(void)
     EnableInterrupts(INTR_FLAG_VCOUNT);
 }
 
-// oops! FRLG commented this out to remove RTC, however Emerald didnt undo this!
-//static void SeedRngWithRtc(void)
-//{
-//    u32 seed = RtcGetMinuteCount();
-//    seed = (seed >> 16) ^ (seed & 0xFFFF);
-//    SeedRng(seed);
-//}
+// oops! FRLG commented this out to remove RTC, however Emerald didnt undo this! e invece
+static void SeedRngWithRtc(void)
+{
+    u32 seed = RtcGetMinuteCount();
+    seed = (seed >> 16) ^ (seed & 0xFFFF);
+    SeedRng(seed);
+}
 
 void InitKeys(void)
 {
